fa567c03b688c970019e8f8cf1d0520265121a46
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-05-14 12:23:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,24 +2,25 @@
 function Car(make, model) {
 	this.make=make;
 	this.model=model;
-
-	
-	}
-	Car.prototype.getMakeModel = function() {
-  return `${this.make} ${this.model}`;
-};
-
+}
+Car.prototype.getMakeModel=function() {
+	return ${this.make} ${this.model};
+	};
 function SportsCar(make, model, topSpeed) {
-this.make=make;
-	this.model=model;
-	this.topSpeed;
-
-
-
+	Car.call(this,make,model);
+	this.topSpeed=topSpeed;
 	
 }
-SportsCar.prototype.getTopSpeed(){
-	return (`${this.topSeed}`);
+SportsCar.prototype=Object.create(Car.prototype);
+SportsCar.prototype.constructor=SportsCar;
+
+SportsCar.prototype.getTopSpeed=function(){
+return this.topSpeed;
+
+
+
+	
+
 }
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
